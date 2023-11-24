b4d77a83b9e0fc2739f7bd2ef890af3db7288d49
_adrianmaradiaga <73617195+AdrianMaradiaga@users.noreply.github.com>
2023-11-24 10:30:29 -0600

    feat: Login & SignUp xaml

---
+++ Cinepolis/Views/LoginPage.xaml.cs
@@ -0,0 +1,24 @@
+namespace Cinepolis.Views;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+
+    private void LoginButton_Clicked(object sender, EventArgs e)
+    {
+        // Puedes navegar a tu página de home aquí.
+    }
+
+    private void ForgotPasswordLinkClicked(object sender, EventArgs e)
+    {
+        // Puedes navegar a tu página de home aquí.
+    }
+
+    private void OnSignUpLinkClicked(object sender, EventArgs e)
+    {
+        // Puedes navegar a tu página de home aquí.
+    }
+}
---
+++ Cinepolis/Views/SignUpPage.xaml.cs
@@ -0,0 +1,21 @@
+namespace Cinepolis.Views;
+
+public partial class SignUpPage : ContentPage
+{
+	public SignUpPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnSignInLinkClicked(object sender, EventArgs e)
+    {
+        // Lógica para manejar el clic en el enlace de inicio de sesión.
+        // Puedes navegar a tu página de inicio de sesión desde aquí.
+    }
+
+    private void SignUpButton_Clicked(object sender, EventArgs e)
+    {
+        // Puedes navegar a tu página de home aquí.
+    }
+
+}
