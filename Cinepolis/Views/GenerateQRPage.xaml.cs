11a592cc0d3b66564b68c16d658c2177832b1a2a
_adrianmaradiaga <73617195+AdrianMaradiaga@users.noreply.github.com>
2023-12-02 16:35:13 -0600

    feat: Generar QR manualmente

---
+++ Cinepolis/Views/GenerateQRPage.xaml.cs
@@ -0,0 +1,38 @@
+using QRCoder;
+namespace Cinepolis.Views;
+
+public partial class GenerateQRPage : ContentPage
+{
+	public GenerateQRPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnGenerateClicked(object sender, EventArgs e)
+    {
+        string pelicula = InputTextPelicula.Text;
+        string hora = InputTextHora.Text;
+        string asiento = InputTextAsiento.Text;
+        string fecha = InputTextFecha.Text;
+
+        string combinedData = $"Nombre de pelicula: {pelicula}, \nHora: {hora}, \nNúmero de asiento: {asiento}, \nFecha: {fecha}";
+
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode(combinedData, QRCodeGenerator.ECCLevel.L);
+        PngByteQRCode qRCode = new PngByteQRCode(qrCodeData);
+        byte[] qrCodeBytes = qRCode.GetGraphic(20);
+        QrCodeImage.Source = ImageSource.FromStream(() => new MemoryStream(qrCodeBytes));
+    }
+
+
+
+
+    private void OnClearFieldsClicked(object sender, EventArgs e)
+    {
+        InputTextPelicula.Text = string.Empty;
+        InputTextHora.Text = string.Empty;
+        InputTextAsiento.Text = string.Empty;
+        InputTextFecha.Text = string.Empty;
+        QrCodeImage.Source = null; // Limpiar la imagen del código QR si es necesario
+    }
+}
--- Cinepolis/Views/LoginPage.xaml.cs
+++ Cinepolis/Views/LoginPage.xaml.cs
@@ -25,7 +25,7 @@ public partial class LoginPage : ContentPage
     {
         if(_userRepository.UserExists()) {
             (_userInfo, _credential) = _userRepository.ReadUser();
-            await Navigation.PushAsync(new NavigationPage(new SelectSnacks()));
+            await Navigation.PushAsync(new NavigationPage(new GenerateQRPage()));
 
         }
     }
@@ -45,7 +45,7 @@ public partial class LoginPage : ContentPage
                 _userRepository.DeleteUser();
             }
             
-            await Navigation.PushAsync(new NavigationPage(new SelectSnacks())); 
+            await Navigation.PushAsync(new NavigationPage(new GenerateQRPage())); 
 
         }
         catch (FirebaseAuthException firebaseAuthException)
