0763e1ff69956866f88741eeca5dc4c9d9b55c85
_adrianmaradiaga <73617195+AdrianMaradiaga@users.noreply.github.com>
2023-12-01 21:03:19 -0600

    feat: Seleccionar Snacks xaml

--- Cinepolis/Models/Snacks.cs
+++ Cinepolis/Models/Snacks.cs
@@ -1,16 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Cinepolis.Models
 {
-    public class Snacks
+    public class Snacks : INotifyPropertyChanged
     {
         public string Name { get; set; }
         public string Description { get; set; }
         public string Price { get; set; }
         public string Image { get; set; }
+
+        private int _quantity;
+        public int Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                _quantity = value;
+                OnPropertyChanged("Quantity"); 
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
---
+++ Cinepolis/Views/SelectSnacks.xaml.cs
@@ -0,0 +1,52 @@
+namespace Cinepolis.Views;
+using Cinepolis.Models;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Input;
+
+public partial class SelectSnacks : ContentPage
+{
+    public ObservableCollection<Snacks> Snacks { get; set; } = new ObservableCollection<Snacks>();
+
+    public SelectSnacks()
+	{
+		InitializeComponent();
+        AddSnacks();
+        BindingContext = this;
+    }
+
+
+    private void AddSnacks()
+    {
+        Snacks.Add(new Snacks { Name = "Combo Palomitas + 2 Refrescos", Description = "Palomitas de maíz + dos refrescos", Price = "L. 120", Image = "https://static.promodescuentos.com/threads/raw/default/667189_1/re/1024x1024/qt/60/667189_1.jpg" });
+        Snacks.Add(new Snacks { Name = "Combo Palomitas + 1 Refresco", Description = "Palomitas de maíz + un refresco", Price = "L. 95", Image = "https://cuponassets.cuponatic-latam.com/backendMx/uploads/imagenes_descuentos/96808/e2d26e632df2f957c70840e07a2d0461a560ed40.XL2.jpg" });
+        Snacks.Add(new Snacks { Name = "Combo Nachos + 1 Refresco", Description = "Nachos + un refresco", Price = "L. 100", Image = "https://cuponassets.cuponatic-latam.com/backendMx/uploads/imagenes_descuentos/99307/6f86a091b0028c31a012a139722ab48e471973b2.XL2.jpg" });
+        Snacks.Add(new Snacks { Name = "Refresco Adicional", Description = "Refresco adicional", Price = "L. 35", Image = "https://static.cinepolis.com/marcas/dulceria/imagenes/productos/9/20179810274538.png" });
+    }
+
+    public ICommand AddItemCommand => new Command<Snacks>(AddItem);
+    public ICommand RemoveItemCommand => new Command<Snacks>(RemoveItem);
+
+    private void AddItem(Snacks snack)
+    {
+        snack.Quantity++;
+        Debug.WriteLine($"Uno mas - Cantidad: {snack.Quantity}");
+        OnPropertyChanged(nameof(Snacks));
+
+    }
+
+    private void RemoveItem(Snacks snack)
+    {
+        Debug.WriteLine($"No resta pero se manda a llamar al metodo: {snack.Quantity}");
+
+        if (snack.Quantity > 0)
+        {
+
+            snack.Quantity--;
+            Debug.WriteLine($"Uno menos - Cantidad: {snack.Quantity}");
+            OnPropertyChanged(nameof(Snacks));
+
+        }
+    }
+
+}
